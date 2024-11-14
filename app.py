c79a2364eea47a155f30cabc99fe62d07edd48c0
venkatesh-annabathula <84910189+venkatesh-annabathula@users.noreply.github.com>
2024-11-14 16:04:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from app import create_app
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from app import routes, models
+    with app.app_context():
+        db.create_all()  # Ensure database tables are created
+
+    app.register_blueprint(routes.bp)
+    return app
 
 app = create_app()
 
