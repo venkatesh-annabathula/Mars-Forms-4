256d70bf8ee9c9e3fcdebf98a2e84776b71b2cfe
venkatesh-annabathula <84910189+venkatesh-annabathula@users.noreply.github.com>
2024-11-14 17:52:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,6 @@
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from app import create_app
 
-db = SQLAlchemy()
+app = create_app()
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-
-    db.init_app(app)
-
-    # Import routes and models only after db and app have been initialized
-    from app.routes import bp as routes_bp
-
-    with app.app_context():
-        db.create_all()  # Ensure database tables are created
-
-    app.register_blueprint(routes_bp)
-    return app
-
-# Entry point for running the app
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
